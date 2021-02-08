d9fc7e2dc04b621be5f142849a8557eae9cd3f63
Josefine1409 <josefine@robl.dk>
2021-02-08 13:00:42 +0100

    Exercise A and B solved thus now printing both hello to world and to the given user

--- Week1/helloWorld.c
+++ Week1/helloWorld.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#define MAX_USERID_LENGTH 32
+
+void printHelloWorld(void){
+	printf("Hello, World!\n");
+}
+
+
+void printHelloName(void){
+	char username[MAX_USERID_LENGTH];
+	cuserid(username);
+	printf("Hello, %s!\n", username);	
+}
 
 int main(void){
-	printf("Hello World\n");
+	printf("----- Exercise A -----\n");
+	printHelloWorld();
+	printf("----- Exercise B -----\n");
+	printHelloName();
 	return 0;
 }
